e7b9cbc48996d88c4a2ced7e31627696b0295aa2
abraxas-hash <217834301+abraxas-hash@users.noreply.github.com>
2026-05-30 17:26:36 -0500

    chore: clean up repository, keeping only the tech discovery form

--- andean-journey-react/test_sheets2.mjs
+++
@@ -1,8 +0,0 @@
-const GOOGLE_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbxyMwt1ywrb74yLEAfF2QNVf6wDGStAV6Qiu1nA5cgkJhEvFq8szcubM_RfpaFeZumbvQ/exec';
-
-async function testFetch() {
-    const response = await fetch(GOOGLE_SHEETS_URL);
-    const json = await response.json();
-    console.log("Root keys:", Object.keys(json));
-}
-testFetch();
